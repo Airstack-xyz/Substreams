d46fea0c4a51b1be72f0a1a812281a16529f62fc
Gopi Bathala <gopi@airstack.xyz>
2023-08-22 17:13:16 +0400

    Fix: PR comments addressed

--- lens-follower-tracker/src/lib.rs
+++ lens-follower-tracker/src/lib.rs
@@ -2,91 +2,99 @@ mod abis;
 mod pb;
 mod helpers;
 
+use helpers::constants::{ZERO_ADDRESS, FOLLOW, UNFOLLOW, EMPTY_STRING};
 use pb::lens::events::{FollowNftDeployed, FollowEvents, FollowNftTransferred};
 use substreams::errors::Error;
-use substreams::{Hex};
+use substreams::Hex;
+use serde::Deserialize;
 use substreams_ethereum::{pb::eth::v2 as eth, Event as EventTrait};
-use helpers::utils::format_with_0x;
+use helpers::utils::{format_with_0x, is_address_valid};
+use anyhow::anyhow;
 
-
-// Lens LPP contract 
-// const LENS_LPP_CONTRACT : &str = "db46d1dc155634fbc732f92e853b10b288ad5a1d";
-const ZERO_ADDRESS : &str = "0x0000000000000000000000000000000000000000";
-const LENS_PROXY_ACTION_ADDRESS : &str = "0xdd3f6c22ecc68007cc9f76da18984995da4b7b82";
-const FOLLOW : &str = "follow";
-const UNFOLLOW : &str = "unfollow";
 
 substreams_ethereum::init!();
 
+
+#[derive(Deserialize)]
+struct InputFilters {
+    lpp_contract_address: Option<String>,
+    proxy_action_contract_address: Option<String>,
+}
+
 /// Extracts lens events from the contract
 #[substreams::handlers::map]
-fn map_lens_follower_events(contract_address: String, blk: eth::Block) -> Result<FollowEvents, Error> {
+fn map_lens_follower_events(params: String, blk: eth::Block) -> Result<FollowEvents, Vec<Error>> {
+    let filters = parse_filters_from_params(params)?;
+    let lpp_contract_address = filters.lpp_contract_address.expect("lpp_contract_address is already verified");
+    let proxy_action_contract_address = filters.proxy_action_contract_address.expect("proxy_action_contract_address already verified");
     let mut follow_nft_deployed_events = vec![];
     let mut follow_nft_transferred_events: Vec<FollowNftTransferred> = vec![];
 
     for log in blk.logs() {
+        let tx_hash = Hex(log.receipt.transaction.hash.clone()).to_string();
         let addres : String = Hex(log.address().clone()).to_string();
-        if !contract_address.contains(&addres) {
+        if !lpp_contract_address.contains(&addres) {
            continue;
         }
         // for FollowNftDeployed event
         if let Some(event) = abis::lens_events::events::FollowNftDeployed::match_and_decode(log) {
             let follow_nft_deployed_event : FollowNftDeployed = FollowNftDeployed {
                 follow_profile_id: event.profile_id.to_string(),
-                follow_token_address: helpers::utils::format_with_0x(Hex(event.follow_nft).to_string())
+                follow_token_address: format_with_0x(Hex(event.follow_nft).to_string()),
+                activity_timestamp: event.timestamp.to_string(),
+                transaction_hash: helpers::utils::format_with_0x(tx_hash.clone()),
+                log_index: log.block_index(),
+                block_number:  blk.number,
             };
-            follow_nft_deployed_events.push(follow_nft_deployed_event)
+            follow_nft_deployed_events.push(follow_nft_deployed_event);
+            continue;
         } 
 
         // for Followed event
         if let Some(event) = abis::lens_events::events::FollowNftTransferred::match_and_decode(log) {
-            
             let from = format_with_0x(Hex(event.from).to_string());
             let to = format_with_0x(Hex(event.to).to_string());
+            let mut follow_nft_transferred_event : FollowNftTransferred = FollowNftTransferred {
+                follow_profile_id: event.profile_id.to_string(),
+                follow_token_id: event.follow_nft_id.to_string(),
+                activity_timestamp: event.timestamp.to_string(),
+                transaction_hash: helpers::utils::format_with_0x(tx_hash.clone()),
+                log_index: log.block_index(),
+                block_number:  blk.number,
+                ..Default::default() 
+            };
             
             // scenario 1: follow event [from:0x to: follower address]
-            if from == ZERO_ADDRESS && to != LENS_PROXY_ACTION_ADDRESS {
-                let follow_nft_transferred_event : FollowNftTransferred = FollowNftTransferred {
-                    follow_profile_id: event.profile_id.to_string(),
-                    follow_token_id: event.follow_nft_id.to_string(),
-                    follower_address: to.clone(),
-                    follow_type: FOLLOW.to_string(),
-                };
-                follow_nft_transferred_events.push(follow_nft_transferred_event) 
+            // example tx hash: 0xdea7fee50dd48d3a6b03d5ed39e41a4df68b311a58d2f38b47e88d721c85a979
+            if from == ZERO_ADDRESS && to != proxy_action_contract_address {
+                follow_nft_transferred_event.follower_address = to.clone();
+                follow_nft_transferred_event.follow_type =  FOLLOW.to_string();    
             }
 
             // scenario 2: follow for event [from:0x..782 [proxy] to: follower address]
-            if from == LENS_PROXY_ACTION_ADDRESS && to != ZERO_ADDRESS {
-                let follow_nft_transferred_event : FollowNftTransferred = FollowNftTransferred {
-                    follow_profile_id: event.profile_id.to_string(),
-                    follow_token_id: event.follow_nft_id.to_string(),
-                    follower_address: to.clone(),
-                    follow_type: FOLLOW.to_string(),
-                };
-                follow_nft_transferred_events.push(follow_nft_transferred_event) 
+            // example tx hash: 0xbc3c64e31c901f0560fb55e1ff8635d91b5a7ab8f5d5b5d50b6053a3907b1f10
+            if from == proxy_action_contract_address && to != ZERO_ADDRESS {
+                follow_nft_transferred_event.follower_address = to.clone();
+                follow_nft_transferred_event.follow_type =  FOLLOW.to_string();  
             }
 
             // scenario 3: burn event [from:follower to: 0x]
-            if from != LENS_PROXY_ACTION_ADDRESS && to == ZERO_ADDRESS {
-                let follow_nft_transferred_event : FollowNftTransferred = FollowNftTransferred {
-                    follow_profile_id: event.profile_id.to_string(),
-                    follow_token_id: event.follow_nft_id.to_string(),
-                    follower_address: from.clone(),
-                    follow_type: UNFOLLOW.to_string(),
-                };
-                follow_nft_transferred_events.push(follow_nft_transferred_event) 
+            // example tx hash: 0x68d80b53990cc9b3d7e48d6c11295f2ede7cbff8ae6a05b7c265bcccf9d994bc
+            if from != proxy_action_contract_address && to == ZERO_ADDRESS {
+                follow_nft_transferred_event.follower_address = from.clone();
+                follow_nft_transferred_event.follow_type =  UNFOLLOW.to_string();               
             }
 
             // scenario 4: transfer between addresses  -> should unfollow the from address
-            if from != ZERO_ADDRESS && from != LENS_PROXY_ACTION_ADDRESS 
-                && to != ZERO_ADDRESS && to != LENS_PROXY_ACTION_ADDRESS {
-                    let follow_nft_transferred_event : FollowNftTransferred = FollowNftTransferred {
-                        follow_profile_id: event.profile_id.to_string(),
-                        follow_token_id: event.follow_nft_id.to_string(),
-                        follower_address: from.clone(),
-                        follow_type: UNFOLLOW.to_string(),
-                    };
-                    follow_nft_transferred_events.push(follow_nft_transferred_event) 
+            // example tx hash: 0xbac904222e15b792acd053f86a8834ebbb4414bedbf4051b0297c378bde73f38
+            if from != ZERO_ADDRESS && from != proxy_action_contract_address 
+                && to != ZERO_ADDRESS && to != proxy_action_contract_address {
+                    follow_nft_transferred_event.follower_address = from.clone();
+                    follow_nft_transferred_event.follow_type =  UNFOLLOW.to_string();                    
+            }
+            // finally push the data to vector
+            if follow_nft_transferred_event.follow_type != EMPTY_STRING {
+                follow_nft_transferred_events.push(follow_nft_transferred_event); 
             }
         }
     }
@@ -95,4 +103,36 @@ fn map_lens_follower_events(contract_address: String, blk: eth::Block) -> Result
         follow_nft_deployed_events,
         follow_nft_transferred_events
     })
+}
+
+fn parse_filters_from_params(params: String) -> Result<InputFilters, Vec<substreams::errors::Error>> {
+    let parsed_result = serde_qs::from_str(&params);
+    if parsed_result.is_err() {
+        return Err(Vec::from([anyhow!("Unpexcted error while parsing parameters")]));
+    }
+
+    let filters = parsed_result.unwrap();
+    verify_filters(&filters)?;
+
+    Ok(filters)
+}
+
+fn verify_filters(params: &InputFilters) -> Result<(), Vec<substreams::errors::Error>> {
+    let mut errors: Vec<substreams::errors::Error> = Vec::new();
+
+    let lpp_contract_address = params.lpp_contract_address.as_ref().unwrap();
+    if !is_address_valid(lpp_contract_address) {
+        errors.push(anyhow!("'lpp_contract_address' address ({}) is not valid", lpp_contract_address));
+    }
+    
+    let proxy_action_contract_address = params.proxy_action_contract_address.as_ref().unwrap();
+    if !is_address_valid(proxy_action_contract_address) {
+        errors.push(anyhow!("'proxy_action_contract_address' address ({}) is not valid", proxy_action_contract_address));
+    }
+
+    if errors.len() > 0 {
+        return Err(errors);
+    }
+
+    Ok(())
 }
--- lens-follower-tracker/src/pb/lens.events.rs
+++ lens-follower-tracker/src/pb/lens.events.rs
@@ -14,6 +14,14 @@ pub struct FollowNftDeployed {
     pub follow_profile_id: ::prost::alloc::string::String,
     #[prost(string, tag="2")]
     pub follow_token_address: ::prost::alloc::string::String,
+    #[prost(string, tag="3")]
+    pub activity_timestamp: ::prost::alloc::string::String,
+    #[prost(string, tag="4")]
+    pub transaction_hash: ::prost::alloc::string::String,
+    #[prost(uint32, tag="5")]
+    pub log_index: u32,
+    #[prost(uint64, tag="6")]
+    pub block_number: u64,
 }
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
@@ -25,6 +33,14 @@ pub struct FollowNftTransferred {
     #[prost(string, tag="3")]
     pub follower_address: ::prost::alloc::string::String,
     #[prost(string, tag="4")]
+    pub activity_timestamp: ::prost::alloc::string::String,
+    #[prost(string, tag="5")]
     pub follow_type: ::prost::alloc::string::String,
+    #[prost(string, tag="6")]
+    pub transaction_hash: ::prost::alloc::string::String,
+    #[prost(uint32, tag="7")]
+    pub log_index: u32,
+    #[prost(uint64, tag="8")]
+    pub block_number: u64,
 }
 // @@protoc_insertion_point(module)
