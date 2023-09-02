6eba59713276aefa5c87c9587b634e071a288e6c
Gopi Bathala <gopi@airstack.xyz>
2023-09-03 00:35:08 +0400

    PR feedback

--- lens-follower-tracker/src/lib.rs
+++ lens-follower-tracker/src/lib.rs
@@ -2,24 +2,26 @@ mod abis;
 mod pb;
 mod helpers;
 
-use helpers::constants::{FOLLOW, UNFOLLOW};
+use helpers::constants::{FOLLOW, UNFOLLOW, ZERO_ADDRESS, EMPTY_STRING};
 use pb::lens::events::{FollowNftDeployed, FollowEvents, FollowNftTransferred};
 use substreams::errors::Error;
-use substreams::{Hex, log};
+use substreams::{Hex};
 use serde::Deserialize;
 use substreams_ethereum::{pb::eth::v2 as eth, Event as EventTrait};
 use helpers::utils::{format_with_0x, is_address_valid};
 use anyhow::anyhow;
 use std::collections::HashMap;
 
-
-
 substreams_ethereum::init!();
-
 
 #[derive(Deserialize)]
 struct InputFilters {
     lpp_contract_address: Option<String>,
+}
+#[derive(Deserialize)]
+struct TransferInfo {
+    from: String,
+    to: String,
 }
 
 /// Extracts lens events from the contract
@@ -29,20 +31,21 @@ fn map_lens_follower_events(params: String, blk: eth::Block) -> Result<FollowEve
     let lpp_contract_address = filters.lpp_contract_address.expect("lpp_contract_address is already verified");
     let mut follow_nft_deployed_events: Vec<FollowNftDeployed> = vec![];
     let mut follow_nft_transferred_events: Vec<FollowNftTransferred> = vec![];
-    // this maps contains follower address for a profileId and followNFTId combination 
-    // key : profileId-followNFTId
-    // value : followerAddress
-    let mut profile_followers_map: HashMap<String, String> = HashMap::new();
     
     // this maps contains follow_nft_transferred_events for a profileId and followNFTId combination 
     // key : profileId-followNFTId
-    // value : followerAddress
+    // value : FollowNftTransferred struct
     let mut profile_follow_nft_transferred_events_map: HashMap<String, FollowNftTransferred> = HashMap::new();
 
-    // this maps contains followed events for a profileId 
-    // key : profileId
-    // value : true /false
-    let mut profile_followed_event_exists_map: HashMap<String, bool> = HashMap::new();
+    // this maps contains number of times followNFTTransferred events occured for a profileId-tokenId combination
+    // key : profileId-followNFTId
+    // value : integer count
+    let mut profile_follow_nft_transferred_events_count_map: HashMap<String, u32> = HashMap::new();
+
+    // this maps contains number of times followNFTTransferred events occured for a profileId-tokenId combination
+    // key :  profileId-followNFTId
+    // value : Transfer
+    let mut profile_follow_nft_transferred_transaction_map: HashMap<String, TransferInfo> = HashMap::new();
 
     // below block will process the follow related events
     for log in blk.logs() {
@@ -66,23 +69,33 @@ fn map_lens_follower_events(params: String, blk: eth::Block) -> Result<FollowEve
         } 
 
         // for Followed event
-        // scenario 1: follow event [from:0x to: follower address]
+        // scenario 1: follow event [from:0x0  to: follower address]
         // example tx hash: 0xdea7fee50dd48d3a6b03d5ed39e41a4df68b311a58d2f38b47e88d721c85a979
 
-        // scenario 2: follow for event [from:0x..782 [proxy] to: follower address]
+        // scenario 2: follow for event [from:0x0  to: proxy address] [from: proxy to: follower address]
         // example tx hash: 0xbc3c64e31c901f0560fb55e1ff8635d91b5a7ab8f5d5b5d50b6053a3907b1f10
 
-        // scenario 3: burn event [from:follower to: 0x]
+        // scenario 3: burn event [from:follower address to: 0x0 ]
         // example tx hash: 0x68d80b53990cc9b3d7e48d6c11295f2ede7cbff8ae6a05b7c265bcccf9d994bc
 
         // scenario 4: transfer between addresses  -> should unfollow the from address
         // example tx hash: 0xbac904222e15b792acd053f86a8834ebbb4414bedbf4051b0297c378bde73f38
 
-        if let Some(event) = abis::lens_events::events::FollowNftTransferred::match_and_decode(log) {            
+        if let Some(event) = abis::lens_events::events::FollowNftTransferred::match_and_decode(log) {   
             let from = format_with_0x(Hex(event.from).to_string());
             let to = format_with_0x(Hex(event.to).to_string());
-            let key = format!("{}{}{}", event.profile_id, "-",event.follow_nft_id);
-            profile_followers_map.insert(key.clone(), to.clone());
+            let key: String = format!("{}{}{}", event.profile_id, "-",event.follow_nft_id);
+
+            // computing the count of nft transferred events for a profileId and NftId combination
+            if let Some(value) = profile_follow_nft_transferred_events_count_map.get_mut(&key) {
+                *value += 1;
+            } else {
+                profile_follow_nft_transferred_events_count_map.insert(key.clone(), 1);
+            }
+
+            // add from, to address to map
+            profile_follow_nft_transferred_transaction_map.insert(key.clone(), TransferInfo{from:from.clone(), to: to.clone()});
+          
            
             let follow_nft_transferred_event : FollowNftTransferred = FollowNftTransferred {
                 follow_profile_id: event.profile_id.to_string(),
@@ -91,39 +104,44 @@ fn map_lens_follower_events(params: String, blk: eth::Block) -> Result<FollowEve
                 transaction_hash: helpers::utils::format_with_0x(tx_hash.clone()),
                 log_index: log.block_index().to_string(),
                 block_number:  blk.number,
-                follower_address: from,
                 ..Default::default() 
             };
-
-            profile_follow_nft_transferred_events_map.insert(key, follow_nft_transferred_event);
-
-            continue;
+            // add nft_transferred events to the map
+            profile_follow_nft_transferred_events_map.insert(key.clone(), follow_nft_transferred_event);
         }
-         // for Followed event
-         if let Some(event) = abis::lens_events::events::Followed::match_and_decode(log) {
-            for profile_id in event.profile_ids {
-                let key = format!("{}", profile_id);
-                log::info!("tx_hash: {} , profile_id {} has follow event", tx_hash, key);
-                profile_followed_event_exists_map.insert(key, true);
-            }
-        } 
     }
 
     // checks to update the correct follower address
-    for (key, value)  in  profile_followers_map.iter() {
-        let follower_address = value.clone();
-        let mut follow_nft_transferred_event = profile_follow_nft_transferred_events_map.get(key).unwrap().clone();
-        let is_followed_event_exists = *profile_followed_event_exists_map.get(&follow_nft_transferred_event.follow_profile_id).unwrap_or(&false);
-            // for actual follow events, we will have below is_followed_event_exists as true
-        if is_followed_event_exists {
-            follow_nft_transferred_event.follower_address = follower_address;
-            follow_nft_transferred_event.follow_type = FOLLOW.to_string();
-        } else {
-            // if follower address is zero (or)
-            // in the case of nft transfer from sender to receiver, then we have already set follower_address as sender_address above
-            // so no need to set follower_address again
-            follow_nft_transferred_event.follow_type = UNFOLLOW.to_string();
+    for (key, value)  in  profile_follow_nft_transferred_events_map.iter() {
+        let mut follow_nft_transferred_event = value.clone();
+        let profile_follow_nft_transferred_event_count = profile_follow_nft_transferred_events_count_map.get(key).unwrap_or(&0);
+        let follow_nft_transferred_transaction_map = profile_follow_nft_transferred_transaction_map.get(key).unwrap();
+
+        let from = follow_nft_transferred_transaction_map.from.to_string();
+        let to = follow_nft_transferred_transaction_map.to.to_string();
+        let mut follower_address = EMPTY_STRING.to_string();
+        let mut follow_type = EMPTY_STRING.to_string();
+
+        if *profile_follow_nft_transferred_event_count == 1  {
+            if from == ZERO_ADDRESS  && to != ZERO_ADDRESS {  // scenario: 1
+                follower_address = to;
+                follow_type = FOLLOW.to_string();
+            } else if from != ZERO_ADDRESS  && to == ZERO_ADDRESS {  // scenario: 3
+                follower_address = from;
+                follow_type = UNFOLLOW.to_string();
+            } else if from != ZERO_ADDRESS && to != ZERO_ADDRESS  { // scenario: 4
+                follower_address = from;
+                follow_type = UNFOLLOW.to_string();
+            }
+        } else if *profile_follow_nft_transferred_event_count > 1{
+            if from != ZERO_ADDRESS && to != ZERO_ADDRESS { // scenario: 2
+                follower_address = to;
+                follow_type = FOLLOW.to_string();
+            }
         }
+        // finally, set follower address and follow type to struct and push to vector.
+        follow_nft_transferred_event.follower_address = follower_address;
+        follow_nft_transferred_event.follow_type = follow_type;
         follow_nft_transferred_events.push(follow_nft_transferred_event);
     }
 
