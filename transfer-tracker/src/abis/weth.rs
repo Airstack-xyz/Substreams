1b22ab1715a405b53f6f3056be49f4bb8a14c95e
Hrishikesh Thakkar <kesh5210@gmail.com>
2024-02-10 00:08:02 +0530

    Making Change to Live Substream to track wETH

---
+++ transfer-tracker/src/abis/weth.rs
@@ -0,0 +1,202 @@
+    const INTERNAL_ERR: &'static str = "`ethabi_derive` internal error";
+    /// Contract's functions.
+    #[allow(dead_code)]
+    #[allow(unused_variables)]
+    pub mod functions {
+        use super::INTERNAL_ERR;
+    }
+    /// Contract's events.
+    #[allow(dead_code)]
+    pub mod events {
+        use super::INTERNAL_ERR;
+        #[derive(Debug, Clone, PartialEq)]
+        pub struct Deposit {
+            pub dst: Vec<u8>,
+            pub wad: ethabi::Uint,
+        }
+        impl Deposit {
+            const TOPIC_ID: [u8; 32] = [
+                225u8,
+                255u8,
+                252u8,
+                196u8,
+                146u8,
+                61u8,
+                4u8,
+                181u8,
+                89u8,
+                244u8,
+                210u8,
+                154u8,
+                139u8,
+                252u8,
+                108u8,
+                218u8,
+                4u8,
+                235u8,
+                91u8,
+                13u8,
+                60u8,
+                70u8,
+                7u8,
+                81u8,
+                194u8,
+                64u8,
+                44u8,
+                92u8,
+                92u8,
+                201u8,
+                16u8,
+                156u8,
+            ];
+            pub fn match_log(log: &substreams_ethereum::pb::eth::v2::Log) -> bool {
+                if log.topics.len() != 2usize {
+                    return false;
+                }
+                if log.data.len() != 32usize {
+                    return false;
+                }
+                return log.topics.get(0).expect("bounds already checked").as_ref()
+                    == Self::TOPIC_ID;
+            }
+            pub fn decode(
+                log: &substreams_ethereum::pb::eth::v2::Log,
+            ) -> Result<Self, String> {
+                let mut values = ethabi::decode(
+                        &[ethabi::ParamType::Uint(256usize)],
+                        log.data.as_ref(),
+                    )
+                    .map_err(|e| format!("unable to decode log.data: {}", e))?;
+                values.reverse();
+                Ok(Self {
+                    dst: ethabi::decode(
+                            &[ethabi::ParamType::Address],
+                            log.topics[1usize].as_ref(),
+                        )
+                        .map_err(|e| {
+                            format!(
+                                "unable to decode param 'dst' from topic of type 'address': {}",
+                                e
+                            )
+                        })?
+                        .pop()
+                        .expect(INTERNAL_ERR)
+                        .into_address()
+                        .expect(INTERNAL_ERR)
+                        .as_bytes()
+                        .to_vec(),
+                    wad: values
+                        .pop()
+                        .expect(INTERNAL_ERR)
+                        .into_uint()
+                        .expect(INTERNAL_ERR),
+                })
+            }
+        }
+        impl substreams_ethereum::Event for Deposit {
+            const NAME: &'static str = "Deposit";
+            fn match_log(log: &substreams_ethereum::pb::eth::v2::Log) -> bool {
+                Self::match_log(log)
+            }
+            fn decode(
+                log: &substreams_ethereum::pb::eth::v2::Log,
+            ) -> Result<Self, String> {
+                Self::decode(log)
+            }
+        }
+        #[derive(Debug, Clone, PartialEq)]
+        pub struct Withdrawal {
+            pub src: Vec<u8>,
+            pub wad: ethabi::Uint,
+        }
+        impl Withdrawal {
+            const TOPIC_ID: [u8; 32] = [
+                127u8,
+                207u8,
+                83u8,
+                44u8,
+                21u8,
+                240u8,
+                166u8,
+                219u8,
+                11u8,
+                214u8,
+                208u8,
+                224u8,
+                56u8,
+                190u8,
+                167u8,
+                29u8,
+                48u8,
+                216u8,
+                8u8,
+                199u8,
+                217u8,
+                140u8,
+                179u8,
+                191u8,
+                114u8,
+                104u8,
+                169u8,
+                91u8,
+                245u8,
+                8u8,
+                27u8,
+                101u8,
+            ];
+            pub fn match_log(log: &substreams_ethereum::pb::eth::v2::Log) -> bool {
+                if log.topics.len() != 2usize {
+                    return false;
+                }
+                if log.data.len() != 32usize {
+                    return false;
+                }
+                return log.topics.get(0).expect("bounds already checked").as_ref()
+                    == Self::TOPIC_ID;
+            }
+            pub fn decode(
+                log: &substreams_ethereum::pb::eth::v2::Log,
+            ) -> Result<Self, String> {
+                let mut values = ethabi::decode(
+                        &[ethabi::ParamType::Uint(256usize)],
+                        log.data.as_ref(),
+                    )
+                    .map_err(|e| format!("unable to decode log.data: {}", e))?;
+                values.reverse();
+                Ok(Self {
+                    src: ethabi::decode(
+                            &[ethabi::ParamType::Address],
+                            log.topics[1usize].as_ref(),
+                        )
+                        .map_err(|e| {
+                            format!(
+                                "unable to decode param 'src' from topic of type 'address': {}",
+                                e
+                            )
+                        })?
+                        .pop()
+                        .expect(INTERNAL_ERR)
+                        .into_address()
+                        .expect(INTERNAL_ERR)
+                        .as_bytes()
+                        .to_vec(),
+                    wad: values
+                        .pop()
+                        .expect(INTERNAL_ERR)
+                        .into_uint()
+                        .expect(INTERNAL_ERR),
+                })
+            }
+        }
+        impl substreams_ethereum::Event for Withdrawal {
+            const NAME: &'static str = "Withdrawal";
+            fn match_log(log: &substreams_ethereum::pb::eth::v2::Log) -> bool {
+                Self::match_log(log)
+            }
+            fn decode(
+                log: &substreams_ethereum::pb::eth::v2::Log,
+            ) -> Result<Self, String> {
+                Self::decode(log)
+            }
+        }
+    }
--- transfer-tracker/src/lib.rs
+++ transfer-tracker/src/lib.rs
@@ -151,6 +151,78 @@ fn map_transfers(blk: eth::Block) -> Result<Transfers, Error> {
             };
             erc1155_transfer_singles.push(erc1155_transfer_single);
         }
+        
+        let token_address = helpers::utils::format_with_0x(
+            Hex(log.clone().address()).to_string(),
+        );
+        if token_address == "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2".to_string(){
+            if let Some(event) = abis::weth::events::Deposit::match_and_decode(log) {
+                let erc20_transfer: Transfer = Transfer {
+                    amount: event.wad.to_string(),
+                    token_id: event.wad.to_string(),
+                    token_address: helpers::utils::format_with_0x(
+                        Hex(log.clone().address()).to_string(),
+                    ),
+                    chain_id: 1.to_string(),
+                    log_index: log.block_index(),
+                    source: 1,
+                    transaction_hash: helpers::utils::format_with_0x(tx_hash.clone()),
+                    operator: helpers::utils::format_with_0x(
+                        Hex(log.receipt.transaction.from.clone()).to_string(),
+                    ),
+                    from: String::from("0x0000000000000000000000000000000000000000"),
+                    to: helpers::utils::format_with_0x(Hex(event.dst).to_string()),
+                    token_type: 0,
+                    block_number: blk.number,
+                    block_timestamp: blk
+                        .clone()
+                        .header
+                        .as_ref()
+                        .unwrap()
+                        .timestamp
+                        .as_ref()
+                        .unwrap()
+                        .seconds as u64,
+                    // log_ordinal: log.ordinal(),
+                    ..Default::default()
+                };
+                transfers.push(erc20_transfer);
+            }
+    
+            if let Some(event) = abis::weth::events::Withdrawal::match_and_decode(log) {
+                let erc20_transfer: Transfer = Transfer {
+                    amount: event.wad.to_string(),
+                    token_id: event.wad.to_string(),
+                    token_address: helpers::utils::format_with_0x(
+                        Hex(log.clone().address()).to_string(),
+                    ),
+                    chain_id: 1.to_string(),
+                    log_index: log.block_index(),
+                    source: 1,
+                    transaction_hash: helpers::utils::format_with_0x(tx_hash.clone()),
+                    operator: helpers::utils::format_with_0x(
+                        Hex(log.receipt.transaction.from.clone()).to_string(),
+                    ),
+                    from: helpers::utils::format_with_0x(Hex(event.src).to_string()),
+                    to: String::from("0x0000000000000000000000000000000000000000"),
+                    token_type: 0,
+                    block_number: blk.number,
+                    block_timestamp: blk
+                        .clone()
+                        .header
+                        .as_ref()
+                        .unwrap()
+                        .timestamp
+                        .as_ref()
+                        .unwrap()
+                        .seconds as u64,
+                    // log_ordinal: log.ordinal(),
+                    ..Default::default()
+                };
+                transfers.push(erc20_transfer);
+            }
+        }
+        // Deposit Transfer
     }
     Ok(Transfers {
         transfers,
